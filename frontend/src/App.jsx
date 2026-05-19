50eb0230dffed016306fc9343817458c2a50b818
user <traorec70@icloud.com>
2026-05-19 16:28:35 +0200

    pages projets et groupes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,35 @@
+
+import './App.css'
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Groupes from './pages/Groupes/groupes'
-import Projets from './pages/Projets/projets'
+
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
+
+import Groupes from '../pages/Groupes/groupes'
+import Projets from '../pages/Projets/projets'
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/groupes" element={<Groupes />} />
-        <Route path="/projets" element={<Projets />} />
-      </Routes>
+
+      <div className="app-layout">
+
+        <Sidebar />
+
+        <div className="main-content">
+
+          <Navbar />
+
+          <Routes>
+            <Route path="/groupes" element={<Groupes />} />
+            <Route path="/projets" element={<Projets />} />
+          </Routes>
+
+        </div>
+
+      </div>
+
     </BrowserRouter>
   )
 }
