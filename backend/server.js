4785a5a507c8256889b9adec1b30840ab42b4537
melek yildiz <melekyildiz@macbook-air-de-melek1.home>
2026-05-26 13:08:19 +0200

    final dashboard update

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,58 @@
-const express = require("express");
+const express = require("express")
+const cors = require("cors")
 
-const app = express();
+const app = express()
 
-app.get("/", (req, res) => {
-  res.send("Hello Express !");
-});
+app.use(cors())
 
-app.listen(3000, () => {
-  console.log("Serveur lancé sur http://localhost:3000");
-});
+app.use(express.json())
+
+/* DATABASE TEMPORAIRE */
+
+let tasks = [
+
+  {
+    id: 1,
+    title: "Projet React"
+  },
+
+  {
+    id: 2,
+    title: "UI Dashboard"
+  }
+
+]
+
+/* GET TASKS */
+
+app.get("/api/tasks", (req, res) => {
+
+  res.json(tasks)
+
+})
+
+/* ADD TASK */
+
+app.post("/api/tasks", (req, res) => {
+
+  const newTask = {
+
+    id: Date.now(),
+
+    title: req.body.title
+
+  }
+
+  tasks.push(newTask)
+
+  res.json(newTask)
+
+})
+
+/* SERVER */
+
+app.listen(5000, () => {
+
+  console.log("Server running on port 5000")
+
+})
